0538847e0a795ac7d69170a774ed327aec1b8031
Uriel Guillermo Daniel <45216219+Uriel1989@users.noreply.github.com>
2022-06-15 20:32:35 -0300

    Add files via upload

---
+++ 18-EsPrimoONoEsPrimo 1/18-EsPrimoONoEsPrimo 1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _18_EsPrimoONoEsPrimo_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, a;
+            a = 0;
+
+            Console.WriteLine("Ingrese un numero: ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 0; i <= 100; i++)
+            {
+
+                if (i % a == 0)
+                {
+                    Console.WriteLine("Los numeros que no son primos son: " + i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
